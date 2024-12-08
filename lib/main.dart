196a487beff25cf4b766ca10a4108f74fea3f29f
liempn13 <phamngocliem2003@gmail.com>
2024-12-09 02:10:42 +0700

    save

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qlpm_ckc_mobile_app/views/home_screen.dart';
+import 'package:qlpm_ckc_mobile_app/views/login_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(home: LoginScreen());
   }
 }
